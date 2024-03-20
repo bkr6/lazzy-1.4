8fc65993b2964bd9de458f7af672c2a71d743a22
João Henrique <139931834+bkr6@users.noreply.github.com>
2024-03-20 16:35:32 -0300

    TCC atualizado

---
+++ Tcc Lazzy - 2024 1.0/Assets/js/cabecalho.js
@@ -0,0 +1,63 @@
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("botao");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+
+
+
+
+const userLogado = JSON.parse(localStorage.getItem("userLogado"));
+
+const logado = document.querySelector("#logado");
+logado.id = 'name-conta'
+logado.innerHTML = `Olá <br><a href="/conta.html">${userLogado.nome}</a>`;
+
+function createButton()
+{
+     var btn = document.createElement('BUTTON');
+     btn.id = 'btn-sair'
+     var lbl = document.createTextNode("CLICK ME");        
+     btn.appendChild(lbl);   
+     btn.onclick = function()
+     {
+        localStorage.removeItem("token");
+        localStorage.removeItem("userLogado");
+        window.location.href = "/index.html";
+     }
+     document.body.appendChild(btn);    
+}
+createButton();
+
+function sair() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userLogado");
+  window.location.href = "/index.html";
+}
+
